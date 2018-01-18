feaa1ef784da3e3a2392443e078e8c0f3e0e8775
jezter78 <33082211+jezter78@users.noreply.github.com>
2018-01-18 10:27:53 +0800

    - optimise checkPalindrome code

--- CodeFight/src/checkPalindrome.js
+++ CodeFight/src/checkPalindrome.js
@@ -1,23 +1,17 @@
 function checkPalindrome(inputString) {
     var inputArray = inputString.split("");
     var array_length = inputArray.length;
+    var midpt = Math.floor(array_length/2);
     
-    if(array_length == 1){
-        return true;
-    } else {
-        var forwardString = "";
-        var backwardString = "";
-        var midpt = Math.floor(array_length/2);
-        for(i=0;i<midpt;i++){
-            forwardString += inputArray[i];
-            backwardString += inputArray[array_length-1-i];
+    if(array_length>0){
+         for(i=0;i<midpt;i++){  
+            //compare outermost values 
+            if(inputArray[i]!=inputArray[array_length-1-i]){
+                return false;
+            }
         }
-        
-        if(forwardString == backwardString){
-            return true;            
-        }         
     }    
-    return false;    
+    return true;    
 }
 
 
